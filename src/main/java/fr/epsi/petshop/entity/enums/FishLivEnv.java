1c222d21ec97147b08618641dd6f7144170d4afa
noxa-dev <martin.ornh@proton.me>
2026-02-25 11:55:01 +0100

    enums

---
+++ src/main/java/fr/epsi/petshop/entity/enums/FishLivEnv.java
@@ -0,0 +1,6 @@
+package fr.epsi.petshop.entity.enums;
+
+public enum FishLivEnv {
+    FRESH_WATER,
+    SEA_WATER
+}
---
+++ src/main/java/fr/epsi/petshop/entity/enums/ProdType.java
@@ -0,0 +1,7 @@
+package fr.epsi.petshop.entity.enums;
+
+public enum ProdType {
+    FOOD,
+    ACCESSORY,
+    CLEANING
+}
